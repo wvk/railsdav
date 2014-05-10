2408dd77613d7334f34c47686ca88ea963bb67e4
Andrew Clunis <andrew@orospakr.ca>
2014-05-10 18:53:11 -0400

    Moreover, Flycode expects all props to always be prefixed.
    * This is Flycode does a string match for ":my_property", *assuming*
      (although ignoring the actual value) that the property element
      always has a namespace prefix.

--- lib/railsdav/renderer.rb
+++ lib/railsdav/renderer.rb
@@ -94,6 +94,14 @@ module Railsdav
       @dav.tag! "D:status", status
     end
 
+    # Allows you to `render :webdav => :not_found` and get a 404
+    # status properly embedded inside multi-status
+    def not_found(options = {})
+      render do |dav|
+        status_for 404
+      end
+    end
+
     # Render a WebDAV multistatus response with a "response" element per resource
     # TODO: explain the magic implemented here
     # Arguments:
@@ -154,25 +162,70 @@ module Railsdav
 
         requested_properties ||= response_hash.keys
 
+        # as a workaround for another bug in Flycode's WebDAV module,
+        # which expects (because of how it does element name matching)
+        # that the properties are always returned with a specified
+        # namespace prefix.  Now, the original Railsdav makes a
+        # slightly careless assumption of its own; it just directly
+        # matches element names in its fixed list, counting on (and
+        # there are none so far) collisions between property names
+        # from various name spaces.  As such, it just assumes that if
+        # the property name is discrimated by a namespace, the client
+        # has only done so by putting a non-prefixed `xmlns` attribute
+        # on the propfind properties (ie., it did not try to use a
+        # prefix itself).  Railsdav really should track and match
+        # properties by both name *and* namespace, in order to be
+        # properly compliant.
+
+        # So, back to the Flycode WebDAV bug: we work around this here
+        # by collecting all of the requested properties, gathering the
+        # namespaces from them, and defining them *all* as prefixes.
+
+        # all of our built-in properties are in the DAV namespace
+        # anyway:
+        gathered_namespaces = {"xmlns:lp0" => "DAV:"}
+        @namespace_counter ||= 1
+        
+        namespaced_requested_properties = {}
+
+        requested_properties.each do |prop_name, opts|
+          if prop_name.to_s.include?(":")
+            # see first paragraph of big comment above
+            Rails.logger.warn("DAV propfind prop request contains a namespace prefix; we do NOT handle these properly yet!")
+          end
+          if (!opts.nil?) && opts["xmlns"]
+            gathered_namespaces["xmlns:lp#{@namespace_counter}"] = opts["xmlns"]
+            opts.delete("xmlns")
+            namespaced_requested_properties["lp#{@namespace_counter}:#{prop_name}"] = opts
+            @namespace_counter +=1
+          else
+            # just copy it through; however, we'll assume it's in the
+            # DAV namespace (which we hardcoded to the `lp0` prefix).
+            namespaced_requested_properties["lp0:#{prop_name}"] = opts
+          end
+        end
+
         response_for(resource.url) do |dav|
-          dav.tag! "D:propstat" do
+          dav.tag! "D:propstat", gathered_namespaces do
             status_for hash[:status]
             dav.tag! "D:prop" do
-              requested_properties.each do |prop_name, opts|
+              namespaced_requested_properties.each do |both_prop_name, opts|
+                prop_space_and_name_pair = both_prop_name.split(":")
+                prop_name = prop_space_and_name_pair[1] || prop_space_and_name_pair[0]
                 if prop_val = response_hash[prop_name.to_sym]
                   if prop_val.respond_to? :call
                     if opts
-                      dav.tag! prop_name, opts, &prop_val
+                      dav.tag! both_prop_name, opts, &prop_val
                     else
-                      dav.tag! prop_name, &prop_val
+                      dav.tag! both_prop_name, &prop_val
                     end
                   else
-                    dav.tag! prop_name, prop_val, opts
+                    dav.tag! both_prop_name, prop_val, opts
                   end
                 elsif opts
-                  dav.tag! prop_name, opts
+                  dav.tag! both_prop_name, opts
                 else
-                  dav.tag! prop_name
+                  dav.tag! both_prop_name
                 end
               end # requested_properties.each
             end # dav.prop
